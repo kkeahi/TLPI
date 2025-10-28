93866ab855841684577e296b8581d4021ab18136
kkeahi <trenstonricks@gmail.com>
2025-10-27 17:44:51 -0700

    Git sync

--- 7.4/7-2.c
+++ 7.4/7-2.c
@@ -1,1 +1,24 @@
 // (Advanced) Implement malloc() and free().
+
+#include "/home/trenston/workspace/TLPI/tlpi-book/lib/tlpi_hdr.h"
+
+struct MemoryBlock {
+  void *ptr;
+  int size;
+};
+
+void *_malloc(size_t size) // Returns pointer to allocated memory on success, or NULL on error 
+{
+  return;
+}
+
+void free(void *ptr) 
+{
+  return;
+}
+
+int
+main()
+{
+  printf("Program break: %p", sbrk(0));
+}
